56619b88034555be7f9b0e35533313f1bbaae1eb
krutika <krutik.halwadia@gmail.com>
2025-01-22 16:50:46 +0530

    #

---
+++ src/components/Intro/Intro.jsx
@@ -0,0 +1,105 @@
+import React, { useContext,useEffect, useRef } from "react";
+import "./Intro.css";
+import Vector1 from "../../img/Vector1.png";
+import Vector2 from "../../img/Vector2.png";
+import girl from "../../img/girl.png";
+import glassesimoji from "../../img/glassesimoji.png";
+import thumbup from "../../img/thumbup.png";
+import crown from "../../img/crown.png";
+import FloatinDiv from "../FloatingDiv/FloatingDiv";
+import Github from "../../img/github.png";
+import LinkedIn from "../../img/linkedin.png";
+import Instagram from "../../img/instagram.png";
+import { themeContext } from "../../Context";
+import { motion } from "framer-motion";
+import { Link } from "react-scroll";
+import Resume from './resume.pdf';
+import Typed from "typed.js"; 
+const Intro = () => {
+  // Transition
+  const transition = { duration: 2, type: "spring" };
+
+  // context
+  const theme = useContext(themeContext);
+  const darkMode = theme.state.darkMode;
+
+  return (
+    <div className="Intro" id="Intro">
+      {/* left name side */}
+      <div className="i-left">
+      
+        <div className="i-name">
+          {/* yahan change hy darkmode ka */}
+          <span style={{ color: darkMode ? "white" : "" }}>Hy! I Am</span>
+          <span>Krutika Halwadia</span>
+
+          <span>
+          Frontend Developer with extensive experience in web design and development, specializing in creating responsive, user-friendly interfaces.<br/>
+          Proven track record of delivering high-quality, efficient code and ensuring optimal performance across multiple platforms and devices.<br/>
+          Adept at translating design concepts into seamless, interactive web experiences while maintaining a focus on accessibility, user experience (UX).<br/>
+          Strong proficiency in modern frontend technologies including HTML5, CSS3, JavaScript, and popular frameworks like React. <br/>
+          Dedicated to producing top-notch work, ensuring pixel-perfect designs, fast loading times, and seamless integration with backend systems.<br/>
+          </span>
+        </div>
+  
+        <a href={Resume} download className="button1 ">
+                  <button className="button s-button">Download CV</button>
+        </a>
+
+        {/* social icons */}
+       
+      </div>
+      {/* right image side */}
+      <div className="i-right">
+        <img src={Vector1} alt="" />
+        <img src={Vector2} alt="" />
+        <img src={girl} alt="" style={{ width: '214px' }} />
+        
+        {/* animation */}
+        <motion.img
+          initial={{ left: "-36%" }}
+          whileInView={{ left: "-24%" }}
+          transition={transition}
+          // src={glassesimoji}
+          alt=""
+        />
+
+        <motion.div
+          initial={{ top: "-4%", left: "74%" }}
+          whileInView={{ left: "53%" }}
+          transition={transition}
+          className="floating-div"
+        >
+          <FloatinDiv img={crown} text1="Web" text2="Developer" />
+        </motion.div>
+
+        {/* animation */}
+        <motion.div
+          initial={{ left: "9rem", top: "18rem" }}
+          whileInView={{ left: "0rem" }}
+          transition={transition}
+          className="floating-div"
+        >
+          {/* floatinDiv mein change hy dark mode ka */}
+          {/* <FloatinDiv img={thumbup} text1="Best Design" text2="Award" /> */}
+        </motion.div>
+
+        <div className="blur" style={{ background: "rgb(238 210 255)" }}></div>
+        <div
+          className="blur"
+          style={{
+            background: "#C1F5FF",
+            top: "17rem",
+            width: "21rem",
+            height: "11rem",
+            left: "-9rem",
+          }}
+        ></div>
+
+      </div>
+
+    </div>
+  );
+};
+
+export default Intro;
---
+++ src/components/Services/Services.jsx
@@ -0,0 +1,93 @@
+import React, { useContext } from "react";
+import "./Services.css";
+import Card from "../Card/Card";
+import HeartEmoji from "../../img/heartemoji.png";
+import Glasses from "../../img/glasses.png";
+import Humble from "../../img/humble.png";
+import { themeContext } from "../../Context";
+import { motion } from "framer-motion";
+import { Link } from "react-scroll";
+// import Resume from './resume.pdf';
+
+const Services = () => {
+  // context
+  const theme = useContext(themeContext);
+  const darkMode = theme.state.darkMode;
+
+  // transition
+  const transition = {
+    duration: 1,
+    type: "spring",
+  };
+
+  return (
+    <div className="services" id="services">
+      {/* left side */}
+      <div className="awesome">
+        {/* dark mode */}
+        <span style={{ color: darkMode ? "white" : "" }}>My </span>
+        <span>Experties</span>
+        <span>
+          Lorem ispum is simpley dummy text of printing of printing Lorem
+          <br />
+          ispum is simpley dummy text of printing
+        </span>
+        {/* <a href={Resume} download>
+          <button className="button s-button">Download CV</button>
+        </a> */}
+        <Link to="contact" smooth={true} spy={true}>
+                  <button className="button i-button">Hire me</button>
+        </Link>
+        <div className="blur s-blur1" style={{ background: "#ABF1FF94" }}></div>
+      </div>
+      {/* right */}
+      <div className="cards">
+        {/* first card */}
+        <motion.div
+          initial={{ left: "25rem" }}
+          whileInView={{ left: "14rem" }}
+          transition={transition}
+        >
+          <Card
+            emoji={HeartEmoji}
+            heading={"Design"}
+            detail={"Photoshop, Adobe aftter effects"}
+          />
+        </motion.div>
+        {/* second card */}
+        <motion.div
+          initial={{ left: "-11rem", top: "12rem" }}
+          whileInView={{ left: "-4rem" }}
+          transition={transition}
+        >
+          <Card
+            emoji={Glasses}
+            heading={"Developer"}
+            detail={"Html, Css, JavaScript, React, Nodejs, Express"}
+          />
+        </motion.div>
+        {/* 3rd */}
+        <motion.div
+          initial={{ top: "19rem", left: "25rem" }}
+          whileInView={{ left: "12rem" }}
+          transition={transition}
+        >
+          <Card
+            emoji={Humble}
+            heading={"Software"}
+            detail={
+              "Dreamviewer, visual studio, adobe photoshop, adobe affter effect"
+            }
+            color="rgba(252, 166, 31, 0.45)"
+          />
+        </motion.div>
+        <div
+          className="blur s-blur2"
+          style={{ background: "var(--purple)" }}
+        ></div>
+      </div>
+    </div>
+  );
+};
+
+export default Services;
